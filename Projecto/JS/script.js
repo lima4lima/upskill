4c360eb0946ae2dca92eb9ec02c7e7514e46c053
lima4lima <46171684+lima4lima@users.noreply.github.com>
2020-11-26 17:06:11 +0000

    NOVO

--- Projecto/JS/script.js
+++ Projecto/JS/script.js
@@ -1,16 +1,17 @@
 
-function show(elementID) {
-  
+ function show(elementID) {
+   
     var ele = document.getElementById(elementID);
     if (!ele) {
         alert("no such element");
         return;
     }
 
-    
+   
     var pages = document.getElementsByClassName('page');
     for(var i = 0; i < pages.length; i++) {
         pages[i].style.display = 'none';
     }
+
     ele.style.display = 'block';
 }
